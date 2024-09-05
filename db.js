43fa45b906d4bbc5aae69664a3c3b8613cdcb7c7
LucasMarvilla <pinheiromarvilla1@gmail.com>
2024-09-05 12:24:21 -0300

    adicionando config do db

--- db.js
+++ db.js
@@ -18,7 +18,11 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT // Certifique-se de adicionar a porta do PostgreSQL
+  port: process.env.DB_PORT, // Certifique-se de adicionar a porta do PostgreSQL
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
