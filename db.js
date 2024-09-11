513dff74c5fc6eb4a20fa7ee85b1a212496a4ae4
LucasMarvilla <pinheiromarvilla1@gmail.com>
2024-09-11 10:40:12 -0300

    alteração no db

--- db.js
+++ db.js
@@ -5,24 +5,12 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: 'variaveis.env' });
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // Certifique-se de adicionar a porta do PostgreSQL
-  ssl: {
-    require: true,
-    rejectUnauthorized: false
-  }
+  port: process.env.DB_PORT // Certifique-se de adicionar a porta do PostgreSQL
 });
 
 export default pool;
